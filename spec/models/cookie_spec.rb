8140879a1ebbc7f58e53e1834159d516bc3ffb9c
= <msanchez@shibumi.cl>
2021-03-28 21:56:50 -0300

    update specs

--- spec/models/cookie_spec.rb
+++ spec/models/cookie_spec.rb
@@ -10,4 +10,12 @@ describe Cookie do
   describe "validations" do
     it { is_expected.to validate_presence_of(:storage) }
   end
+  
+  describe "after_create" do
+    let(:cookie) { create(:cookie) }
+
+    it 'is not ready' do
+      expect(cookie.ready?).not_to be_true
+    end
+  end
 end
