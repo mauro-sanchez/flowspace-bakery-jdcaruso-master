ea5050efd2b8ba64d3db02d5729ae6c211ee10a4
= <msanchez@shibumi.cl>
2021-03-29 00:35:52 -0300

    done

--- app/models/oven.rb
+++ app/models/oven.rb
@@ -1,6 +1,10 @@
 class Oven < ActiveRecord::Base
   belongs_to :user
-  has_one :cookie, as: :storage
+  has_many :cookies, as: :storage
 
   validates :user, presence: true
+
+  def all_cookies_cooked?
+    cookies.all?(&:ready?)
+  end
 end
